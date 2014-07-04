f600f7fc5e644c4a89de3f67d3ffeb2e18f39bff
tungnt <nguyenthanhtunghp2511@gmail.com>
2014-07-04 20:07:35 +0700

    Comment

--- Beer.java
+++ Beer.java
@@ -1,8 +1,10 @@
-
+//Bai tap 1
 public class Beer {
 	public static void main (String[]args){
 		int count;
+		//In ra thong tin ca nhan
 		System.out.println("Nguyen Thanh Tung"+'\t'+"11020379"+'\t'+"K56CC"+'\t'+"tungnt561"+'\t'+"tungnt_561@vnu.edu.vn"+'\t');
+		//In ra cac dong theo yeu cau bai toan
 		for (count = 9;count >0;count--){
 			System.out.println(count+" bottles of beer on the wall, "+count+" bottles of beer.");
 			System.out.println("Take one down, pass it around,");
